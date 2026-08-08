2d96080547880c403e8bb3f479839239391f4730
okaka favour <okakafavour81@gmail.com>
2026-08-08 05:50:32 +0100

    users features updated

--- internal/common/model.go
+++ internal/common/model.go
@@ -8,10 +8,10 @@ import (
 )
 
 type BaseModel struct {
-	ID        uuid.UUID      `gorm:"type:uuid;primaryKey" json:"id"`
+	ID        uuid.UUID      `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
 
 func (b *BaseModel) BeforeCreate(tx *gorm.DB) error {
